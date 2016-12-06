e5558733adddf0dc3e83206306cac3bc63f8eda3
Varvara <shinkarenko.varvara@gmail.com>
2016-12-06 16:06:11 +0000

    Creating first methods - better management of students' list

--- directory.rb
+++ directory.rb
@@ -15,13 +15,22 @@ students = [
   "Norman Bates"
 ]
 
-# and then print them
-puts "The students of Villains Academy"
-puts "-------------"
-# On every iteration, the current name will be assigned to the value student that we specified between two vertical bars.
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
-# finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+def print(names)
+  names.each do |name|
+    puts name
+  end # ending the block of code
+end # ending the method
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+# we should call the methods for something to happen
+print_header
+# passing the students variable to the methods as an argument (called "names" in both cases)
+print(students)
+print_footer(students)

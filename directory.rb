7c138c329371497e74692638a4caa13f45d00dbf
Varvara <shinkarenko.varvara@gmail.com>
2016-12-06 16:49:27 +0000

    Asking for user input

--- directory.rb
+++ directory.rb
@@ -1,20 +1,24 @@
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this block of code
+  while !name.empty? do # until the input message is not empty do the following
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    # now, we print a line to let the user know how many students have been entered so far
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
 
-# let's put all the students into an array
-# we're storing the cohort month as a symbol, because we don't expect to treat it as a string
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
 
 def print_header
   puts "The students of Villains Academy"
@@ -30,6 +34,8 @@ end # ending the method
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
+
+students = input_students #array of students returned by input_students is assigned to variable students
 # we should call the methods for something to happen
 print_header
 # passing the students variable to the methods as an argument (called "names" in both cases)

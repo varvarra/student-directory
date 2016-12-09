a79da7dd1167c87a762de795d0d8a2922c50394b
Varvara <shinkarenko.varvara@gmail.com>
2016-12-09 10:04:45 +0000

    13. Taking arguments from the command line

--- directory.rb
+++ directory.rb
@@ -1,51 +1,24 @@
 
 @students = [] # an empty array  accessible to all methods
-def load_students
-  file = File.open("students.csv", "r") # open file for reading
-  # then read all lines into array and iterate over it
-  # using parallel assignment
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(',') # assign  line to the name and cohort variables
-  #once we have the name and cohort we create a new hash and put it into teh list of students
-    @students << {name: name, cohort: cohort.to_sym}
-  end
-  file.close
-end
-
-
-def save_students
-  # open the file for writing
-  file = File.open("students.csv", "w")
-  # iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]] # put all elements of the students hash into array student_data
-    csv_line = student_data.join(",") # convert it to the string and join it together using coma separator
-    file.puts csv_line # writing to the file using method puts
-  end
-  file.close # every time you open a file it needs to be closed
-end
-
-
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
 def interactive_menu
   loop do
     print_menu  # 1. print the menu and ask the user what to do
-    process(gets.chomp) # 2. read the input and save it into a variable
+    process(STDIN.gets.chomp) # 2. read the input and save it into a variable
     #selection = gets.chomp
     # 3. do what the user has asked
   end
@@ -97,5 +70,41 @@ def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]] # put all elements of the students hash into array student_data
+    csv_line = student_data.join(",") # convert it to the string and join it together using coma separator
+    file.puts csv_line # writing to the file using method puts
+  end
+  file.close # every time you open a file it needs to be closed
+end
 
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r") # open file for reading
+  # then read all lines into array and iterate over it
+  # using parallel assignment
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',') # assign  line to the name and cohort variables
+  #once we have the name and cohort we create a new hash and put it into teh list of students
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
